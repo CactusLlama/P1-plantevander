c79cb4ee81ef9343289036af76168b3682bff3fa
CactusLlama <58257787+CactusLlama@users.noreply.github.com>
2021-12-14 12:44:47 +0100

    manually merged and improved setup branch

--- plantData.h
+++ plantData.h
@@ -1,7 +1,21 @@
-typedef struct Plants {    
+struct Plants {    
     char name[20];
-    double mg;
-    double sodium;
-    double ph;
+    float airTempMax;
+    float airTempMin;
+    float waterTempMax;
+    float waterTempMin;
+    float ecMax;
+    float ecMin;
+    float phMax;
+    float phMin;
+};
 
+typedef struct PlantSensors {
+    char name[20];
+    float ph;
+    float ec;
+    float waterLevel;
+    float airTemp;
+    float waterTemp;
+    float humidity;
 } curPlant;
--- plantevander.c
+++ plantevander.c
@@ -4,31 +4,32 @@
 #include <conio.h>
 #include <time.h>
 #include "plantData.h"
+#include "declareFunctions.h"
 
 //time constant for tick in main loop
 #define TIME_CONSTANT_MILSEC 2000
 
-//Declaring functions ahead of use
-void process_input(char a);
-void delay(int mSeconds);
-void invalid_input();
-void read_data(struct Plants *current);
-void overwrite_data(struct Plants *current);
-void edit_data(struct Plants *current, int state);
 
 int main() {
     char kbInput;
     clock_t curTime = 0;
     int state, ctr = 0;
-    struct Plants curPlant;
+    int plantAmount = 1, selectedProfile = 0;
+    struct Plants plants[10];
+    struct PlantSensors curPlant;
+    
     
     //read data from .txt into struct for easier access during runtime
     read_data(&curPlant);     
-
+    
+    init_structs(plants);
+    
+    edit_mode(plants, &plantAmount);
+    
     //endless loop
     for(;;) {
-        printf("\033[%d;%dH", 1, 1);
-
+        system("cls");
+         
         if (ctr % 9 == 0) {
             printf("Sprinklers turned on..\n%d\n", ctr);
             state = 1;
@@ -37,14 +38,14 @@ int main() {
             state = -1;
         }
         
-        printf("Current plant: %s\n", curPlant.name);;
-        printf("    Current magnesium levels: %lf\n    Current sodium levels: %lf\n    Current pH-level: %.1lf\n", curPlant.mg, curPlant.sodium, curPlant.ph);
-        printf("\nEnter the number of which action you want to do.\n 1. Add magnesium\n 2. Add sodium\n 3. Exit program\n");
-     
+        printf("Current plant: %s\nSelected profile: %s\n", curPlant.name, plants[selectedProfile].name);
+        printf("Current sensor data:\n pH value: %.2f\n EC value: %.2f\n Current water level in tank: %.1f L\n Current air temperature: %.1f deg C\n Current water temperature: %.1f deg C\n Current humidity level: %.2f%%\n", curPlant.ph, curPlant.ec, curPlant.waterLevel, curPlant.airTemp, curPlant.waterTemp, curPlant.humidity);
+        printf("\nEnter the number of which action you want to do.\n 1. Enter edit mode\n 2. Enter debugging mode\n 3. Exit program\n");
+        
         //if user presses their keyboard, get the char and process which option the user chose
         if(kbhit()) {
             kbInput = getch();
-            process_input(kbInput);
+            process_input(kbInput, plants, &plantAmount, &curPlant);
         }
         
         edit_data(&curPlant, state);
@@ -65,25 +66,24 @@ int main() {
     return 0;
 }
 
-void process_input(char a) {
+void process_input(char a, struct Plants plants[], int *plantAmount, struct PlantSensors *curPlant) {
     //clear terminal
     system("cls");
 
     //switch statement to see which option the user chose
     switch (a) {
         case '1':
-        printf("magnesium added");
-        delay(500);
+        edit_mode(plants, plantAmount);
         break;
-
+        
         case '2':
-
+        
         break;
+        
         case '3':
         exit(0);
-
         break;
-
+        
         default:
         invalid_input();
     }
@@ -109,54 +109,64 @@ void invalid_input() {
     return;
 }
 
-void read_data(struct Plants *current) {
+void read_data(struct PlantSensors *current) {
     char tempStr[100] = {"placeholder"};
     char prevStr[100] = {"asdf"};
-    double tempNum = 12.0;
+    float tempNum = 12.0;
     FILE *file = fopen("plantData.txt", "r+");
     
     //keep scanning and editing struct var values until input is End Of File
     while (strcmp(prevStr, tempStr) != 0) {
         strcpy(prevStr, tempStr);
-        fscanf(file, "%s %lf\n", &tempStr, &tempNum);
+        fscanf(file, "%s %f\n", &tempStr, &tempNum);
         //number after the name in pplantData.txt is set to -1
         if (tempNum < 0) {
             strcpy(current->name, tempStr);
         }
-        if (strcmp(tempStr, "magnesium") == 0) {
-            current->mg = tempNum;
-        } else if (strcmp(tempStr, "sodium") == 0) {
-            current->sodium = tempNum;
-        } else if (strcmp(tempStr, "ph") == 0) {
+        if (strcmp(tempStr, "ph") == 0) {
             current->ph = tempNum;
+        } else if (strcmp(tempStr, "ec") == 0) {
+            current->ec = tempNum;
+        } else if (strcmp(tempStr, "waterLevel") == 0) {
+            current->waterLevel = tempNum;
+        } else if (strcmp(tempStr, "airTemp") == 0) {
+            current->airTemp = tempNum;
+        } else if (strcmp(tempStr, "waterTemp") == 0) {
+            current->waterTemp = tempNum;
+        } else if (strcmp(tempStr, "humidity") == 0) {
+            current->humidity = tempNum;
         }
     }
     return;
 }
 
 //do the same as readData() but write instead of read
-void overwrite_data(struct Plants *current) {
+void overwrite_data(struct PlantSensors *current) {
     FILE *file = fopen("plantData.txt", "w+");
     int ctr = 0;
     //string array for printing in .txt
-    char order[12][100] = {
-                            "magnesium",
-                            "sodium",
-                            "ph"
+    char order[6][100] = {
+                            "ph",
+                            "ec",
+                            "waterLevel",
+                            "airTemp",
+                            "waterTemp",
+                            "humidity"
                           };
     
-    //behold the wall of fprintf();
-    //there might be a better solution, tho idk how. pointers to the struct didnt work
-    //needs to be updated along with plantData.h and plantData.txt when more nutriens get added
+    //prints this data to .txt file from struct
     fprintf(file, "%s -1\n", current->name);
-    fprintf(file, "%s %lf\n", order[ctr++], current->mg);
-    fprintf(file, "%s %lf\n", order[ctr++], current->sodium);
-    fprintf(file, "%s %lf\n", order[ctr++], current->ph);
+    fprintf(file, "%s %f\n", order[ctr++], current->ph);
+    fprintf(file, "%s %f\n", order[ctr++], current->ec);
+    fprintf(file, "%s %f\n", order[ctr++], current->waterLevel);
+    fprintf(file, "%s %f\n", order[ctr++], current->airTemp);
+    fprintf(file, "%s %f\n", order[ctr++], current->waterTemp);
+    fprintf(file, "%s %f\n", order[ctr++], current->humidity);
 
     return;
 }
 
-void edit_data(struct Plants *current, int state) {
+void edit_data(struct PlantSensors *current, int state) {
     //initialize random number generator
     time_t t;
     srand((unsigned) time(&t));
@@ -166,12 +176,167 @@ void edit_data(struct Plants *current, int state) {
     //needs to be updated when nutrient solution gets implemented
     //also with more nutrients
     //ph and ec should be handled another way, since they depend on other factors
+    /*
     if (state == 1) {
         current->mg += ((double)rand() / RAND_MAX) * 0.35 + 0.15;
     } else {
         current->mg -= ((double)rand() / RAND_MAX) * 0.09 + 0.01;
     }
-
+*/
 
     return;
+}
+
+void edit_mode(struct Plants plants[], int *plantAmount) {
+    char input;
+    int isLoop = 1;
+    int num;
+    
+    while(isLoop) {
+        system("cls");
+        list_plants(plants, *plantAmount);
+        printf("\nType the number of what action you want to take:\n");
+        printf(" 1. View and/or edit a plant's limit values\n 2. Add another plant to the list\n 3. Enter simulation\n");
+        input = getch();
+        
+        switch (input) {
+            case '1':
+            printf("Type the number of the plant you wish to view/edit:\n");
+            input = getch();
+            num = input - '0' - 1;
+            if (*plantAmount > num | 0 >= plantAmount) {
+                printf("Invalid input!");
+                break;
+            }
+            print_plant(plants[num]);
+            printf("\nDo you want to edit the values of this plant? [y/n]\n");
+            input = getch();
+            if (input == 'y') {
+                edit_plant(&plants[num]);
+                printf("Plant edited!");
+                delay(1500);
+            }
+            break;
+            
+            case '2':
+            edit_plant(&plants[*plantAmount]);
+            *plantAmount += 1;
+            printf("Plant added!");
+            delay(1500);
+            break;
+            
+            case '3':
+            isLoop = 0;
+            break;
+            
+            default:
+            printf("\nInvalid input!");
+            break;
+        }
+    }
+}
+
+//print the ranges of selected plant
+void print_plant(struct Plants current) {
+    printf("\nName of this plant: %s\n", current.name);
+    printf("pH values for this plant range from %.2f-%.2f\n", current.phMin, current.phMax);
+    printf("EC levels for this plant range from %.2f-%.2f\n", current.ecMin, current.ecMax);
+    printf("Air temperatures for this plant range from %.1f-%1.f deg C\n", current.airTempMin, current.airTempMax);
+    printf("Water temperatures for this plant range from %.2f-%.2f deg C\n", current.waterTempMin, current.waterTempMax);
+}
+
+//print the names of the current plants stored in array of structs in a list
+void list_plants(struct Plants plants[], int plantAmount) {
+    system("cls");
+    
+    printf("Current plants in system:\n");
+    for (int i = 0; i < plantAmount; i++) {
+        printf(" %d. %s\n", i + 1, plants[i].name);
+    }
+}
+
+//edit the ranges of selected plant
+void edit_plant(struct Plants *current) {  
+    float a, b;
+    
+    system("cls");
+    
+    printf("Enter the name of the plant: ");
+    scanf(" %s", current->name);
+    
+    ph:
+    printf("\nEnter the upper limit of the pH value for this plant: ");
+    scanf(" %f", &a);
+    printf("\nEnter the lower limit of the pH value for this plant: ");
+    scanf(" %f", &b);
+    
+    //check if phMax and phMin are within 0 and 14 and and if phMax is the biggest of the two before assigning
+    if (a > b && (14 - a) >= 0 && (b) >= 0) {
+        current->phMax = a;
+        current->phMin = b;
+    } else {
+        printf("\nInvalid input! Upper limit must be greater than lower limit and they must be between 0 and 14 both inclusive!");
+        delay(1500);
+        system("cls");
+        goto ph;
+    }
+    
+    ec:
+    printf("\nEnter the upper limit of the EC value for this plant: ");
+    scanf(" %f", &a);
+    printf("\nEnter the lower limit of the EC value for this plant: ");
+    scanf(" %f", &b);
+    
+    if (cmp_nums(a, b)) {
+        current->ecMax = a;
+        current->ecMin = b;
+    } else goto ec;
+    
+    airtemp:
+    printf("\nEnter the upper limit of the air temperature for this plant: ");
+    scanf(" %f", &a);
+    printf("\nEnter the lower limit of the air temperature for this plant: ");
+    scanf(" %f", &b);
+    
+    if (cmp_nums(a, b)) {
+        current->airTempMax = a;
+        current->airTempMin = b;
+    } else goto airtemp;
+    
+    watertemp:
+    printf("\nEnter the upper limit of the water temperature for this plant: ");
+    scanf(" %f", &a);
+    printf("\nEnter the lower limit of the water temperature for this plant: ");
+    scanf(" %f", &b);
+    
+    if (cmp_nums(a, b)) {
+        current->waterTempMax = a;
+        current->waterTempMin = b;
+    } else goto watertemp;
+}
+
+//compare floats. ph does not call this since it has to be within a defined range and this only finds the biggest number of two floats
+int cmp_nums(float a, float b) {
+    if (a > b) {
+        return 1;
+    } else {
+        printf("\nInvalid input! Upper limit must be greater than lower limit!");
+        delay(1500);
+        system("cls");
+    }
+}
+
+//predefine the first plant in the array of structs
+void init_structs(struct Plants plants[]) {
+    
+    strcpy(plants[0].name, "Kartoffel");
+    plants[0].airTempMax = 30.0;
+    plants[0].airTempMin = 5.0;
+    plants[0].waterTempMax = 25.0;
+    plants[0].waterTempMin = 12.5;
+    plants[0].phMax = 8.5;
+    plants[0].phMin = 7.3;
+    plants[0].ecMax = 12.5;
+    plants[0].ecMin = 10;
+    
 }
